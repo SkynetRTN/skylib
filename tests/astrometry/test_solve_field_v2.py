f1d06bdde824890106850a47f0119b3cb58e6fed
Joshua Haislip <jhaislip@gmail.com>
2026-01-06 08:26:26 -0500

    .

--- tests/astrometry/test_solve_field_v2.py
+++ tests/astrometry/test_solve_field_v2.py
@@ -172,8 +172,8 @@ def test_solve_field_v2_atlas_samples() -> None:
 
     for sample in samples:
         image_path = _sample_image_path(sample)
-        if "007" not in str(image_path):
-            continue
+        # if "007" not in str(image_path):
+        #     continue
 
         wcs_path = _sample_wcs_path(sample)
         request = _solve_request_from_sample(sample, image_path)
